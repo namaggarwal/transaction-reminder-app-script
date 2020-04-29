36417966fc8d4d7361c41fef6d4e0109e3e19d5a
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-29 20:17:26 +0800

    Added bank

--- bank.ts
+++ bank.ts
@@ -1,39 +1,37 @@
+import GMail from './gmail-app-script/gmail';
+
 interface TransactionProvider {
   getTransactions(): string[]
 }
 
 class StandardChartered implements TransactionProvider {
+  private gmailClient: GMail
+
+  constructor(gmailClient: GMail) {
+    this.gmailClient = gmailClient;
+  }
 
   getTransactions(): string[] {
     const transactions: string[] = [];
-
-    const messages = [
-      {
-        "text": "sdjhsdjhds jhsdjdh Transaction Alert Primary / Supplementary Card Thank you for charging +SGD 8.53 on 26-Apr-20 01:06 PM to yr credit card ****2815 at GIANT EXPRESS-JL MEMBI."
-      },
-      {
-        "text": "sdjhsdjhds jhsdjdh Transaction Alert Primary / Supplementary Card Thank you for charging +SGD 10.53 on 26-Apr-20 01:06 PM to yr credit card ****2815 at GIANT EXPRESS-JL MEMBI."
-      },
-      {
-        "text": "sdjhsdjhds jhsdjdh Transaction Alert Primary / Supplementary Card Thank you for charging +SGD 89.53 on 26-Apr-20 01:06 PM to yr credit card ****2815 at GIANT EXPRESS-JL MEMBI."
-      }
-    ]
-
+    const threads = this.gmailClient.search('label:bank-sc transaction alert primary newer_than:10d');
+    const messages = GMail.getFlattenMessagesFromThreads(threads);
     messages.forEach((message) => {
       const text = StandardChartered.getTransactionText(message.text);
-      if(text) {
+      if (text) {
         transactions.push(text);
       }
     });
-
     return transactions;
   }
 
   private static getTransactionText(text: string): string {
-    const re = /\+([A-Z]+ \d+\.?(\d+)?).*at (.*)/
-    const match = re.exec(text)
+    const re = /\+([A-Z]+ \d+\.?(\d+)?).*at (.*)/;
+    const match = re.exec(text);
     if (Array.isArray(match) && match.length >= 4) {
-      return match[1] + " at " + match[3];
+      return `${match[1]} at ${match[3]}`;
     }
+    return null;
   }
 }
+
+export default StandardChartered;
