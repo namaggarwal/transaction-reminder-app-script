002db14819025520f1891a434a5358392e1d8730
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-30 16:03:01 +0800

    Updated redirect URL

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 const config = {
   folderID: 'AQMkADAwATMwMAItMmE4My1iZjAwAC0wMAItMDAKAC4AAAOjSZ0xhCeGRYFc0mKMvH32AQBO44dJgt_pSLEduAi76GaeAAAAEwpTlQAAAA==',
   clientID: '3681c137-1a75-48cb-8628-fe61ed13dda3',
-  redirectURI: 'https://script.google.com/macros/s/AKfycbwvi9grFvDkT6MoqNJ4r83XYai61zI0FVxcG-9j09QT/dev/REDIRECT',
+  redirectURI: 'https://script.google.com/macros/s/AKfycbxQE2EgjaMc7EiR27V2YkLVEi7Wgmy4kyj7iM-QIcMFGEASjEhY/exec',
   scopes: ['https://outlook.office.com/tasks.readwrite', 'offline_access'],
 };
 
