e3eee8191932f41ce63cbdec1001a437e20c430a
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-30 16:06:35 +0800

    Fixed redirect

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 const config = {
   folderID: 'AQMkADAwATMwMAItMmE4My1iZjAwAC0wMAItMDAKAC4AAAOjSZ0xhCeGRYFc0mKMvH32AQBO44dJgt_pSLEduAi76GaeAAAAEwpTlQAAAA==',
   clientID: '3681c137-1a75-48cb-8628-fe61ed13dda3',
-  redirectURI: 'https://script.google.com/macros/s/AKfycbxQE2EgjaMc7EiR27V2YkLVEi7Wgmy4kyj7iM-QIcMFGEASjEhY/exec',
+  redirectURI: 'https://script.google.com/macros/s/AKfycbxQE2EgjaMc7EiR27V2YkLVEi7Wgmy4kyj7iM-QIcMFGEASjEhY/exec/REDIRECT',
   scopes: ['https://outlook.office.com/tasks.readwrite', 'offline_access'],
 };
 
